26665b7ec261edea95ca0c3fd240c13e9bea5a56
Lewis Long <llongdesigns@gmail.com>
2016-05-10 10:00:57 -0400

    Finished BrowserSync video

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+gulp.task('server', function(){
+  browserSync.init({
+    server: './'
+  });
+});
 
 
 gulp.task('sass', function(){
   return gulp.src('./scss/**/*.scss')
   .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-  .pipe(gulp.dest('./css'));
+  .pipe(gulp.dest('./css'))
+  .pipe(browserSync.stream());
 });
 
-gulp.task('default', function() {
+gulp.task('default',['server'], function() {
   gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch('*.html').on('change', reload);
 });
